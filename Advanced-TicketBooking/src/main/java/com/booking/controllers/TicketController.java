e7c3dfcbfa299fc92cb5cf153ae5fe59020a5d97
rcoder23 <rohitsrivastava2303@gmail.com>
2023-09-11 11:56:00 -0700

    created inital check controller and configured db

---
+++ Advanced-TicketBooking/src/main/java/com/booking/controllers/TicketController.java
@@ -0,0 +1,19 @@
+package com.booking.controllers;
+
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@RequestMapping("api/v1/ticket")
+public class TicketController {
+
+    @GetMapping("/check")
+    public ResponseEntity<String> justCheck(){
+        return ResponseEntity.ok("controller");
+    }
+
+}
